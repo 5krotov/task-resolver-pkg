952a90d26c1aa86cb8cc6ced52551e6bdef28d82
IWillBurn <eugene.boyarnikov@gmail.com>
2025-04-25 21:05:39 +0300

    Add statuses

--- entity/v1/status.go
+++ entity/v1/status.go
@@ -6,3 +6,10 @@ type Status struct {
 	Status    int       `json:"status"`
 	Timestamp time.Time `json:"timestamp"`
 }
+
+const (
+	STATUS_CREATED    = 0
+	STATUS_PENDING    = 1
+	STATUS_INPROGRESS = 2
+	STATUS_DONE       = 3
+)
